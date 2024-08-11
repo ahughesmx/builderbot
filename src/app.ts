80cb9862e973c201e4eb4ea659d661875c778eb1
ahughesmx <127898469+ahughesmx@users.noreply.github.com>
2024-08-10 19:13:57 -0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT ?? 3008;
 const ASSISTANT_ID = process.env.ASSISTANT_ID ?? "";
 
 // Enlace de Google Maps predefinido
-const googleMapsLink = "https://www.google.com/maps/place/Eiffel+Tower/@48.8584,2.2945,17z"; // Reemplaza con tu enlace
+const googleMapsLink = "https://g.co/kgs/ZJgZ112"; // Reemplaza con tu enlace
 
 // Palabras clave para responder con el enlace de ubicación
 const locationKeywords: [string, ...string[]] = ["dirección", "localización", "domicilio", "ubicación", "mapa"];
